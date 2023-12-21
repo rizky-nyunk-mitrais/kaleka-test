78bc8425ad7bc45cc606fc1bbd15e564678d7071
Rizky Anas Bukhori <Rizky_A421@mitrais.com>
2023-12-21 10:55:10 +0700

    making the routing

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,33 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import * as React from "react";
+import * as ReactDOM from "react-dom/client";
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import "./index.css";
+import {
+  Dashboard,
+  Detail,
+  About
+} from './pages'
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Dashboard/>,
+  },
+  {
+    path: "/coordinate",
+    element: <Detail/>,
+  },
+  {
+    path: "/about",
+    element: <About/>,
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
---
+++ src/pages/index.ts
@@ -0,0 +1,9 @@
+import Dashboard from "./Dashboard";
+import Detail from "./Detail";
+import About from "./About";
+
+export {
+  Dashboard,
+  Detail,
+  About
+}
